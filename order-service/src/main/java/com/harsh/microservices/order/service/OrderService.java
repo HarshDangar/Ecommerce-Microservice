571f754fe0a91021d18ae3c38a21fe76bb6f85c1
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-06-15 14:44:45 +0530

    Completed orderservice controller service and repository

---
+++ order-service/src/main/java/com/harsh/microservices/order/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.harsh.microservices.order.service;
+
+import com.harsh.microservices.order.dto.OrderRequest;
+import com.harsh.microservices.order.model.Order;
+import com.harsh.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+    }
+}
