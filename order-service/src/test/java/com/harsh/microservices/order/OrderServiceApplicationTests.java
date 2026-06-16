813afcaf71dfc950a6f8720b0b3ff1ceca9d3f87
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-06-16 14:19:14 +0530

    Done test using WireMock

--- order-service/src/test/java/com/harsh/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/harsh/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.harsh.microservices.order;
 
+import com.harsh.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,12 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0) //0 meaning springboot will pick random port for running wiremock server
 class OrderServiceApplicationTests {
 
     @ServiceConnection
@@ -40,6 +43,8 @@ class OrderServiceApplicationTests {
                      "quantity": 1
                 }
                 """;
+
+        InventoryClientStub.stubInventoryCall("iPhone_15", 1);
 
         var responseBodyString = RestAssured.given()
                 .contentType("application/json")
