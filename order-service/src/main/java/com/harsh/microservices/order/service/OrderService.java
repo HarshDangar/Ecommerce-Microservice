a5b02cde52c74bfd3dfbbfd643fdc5d536aa51b9
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-06-18 15:24:34 +0530

    Implement Kafka and created notification service to send mail to the user

--- order-service/src/main/java/com/harsh/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/harsh/microservices/order/service/OrderService.java
@@ -2,19 +2,24 @@ package com.harsh.microservices.order.service;
 
 import com.harsh.microservices.order.client.InventoryClient;
 import com.harsh.microservices.order.dto.OrderRequest;
+import com.harsh.microservices.order.event.OrderPlacedEvent;
 import com.harsh.microservices.order.model.Order;
 import com.harsh.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
 
@@ -26,8 +31,17 @@ public class OrderService {
             order.setPrice(orderRequest.price());
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
-
             orderRepository.save(order);
+
+            //Send message to Kafka Topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
         }
